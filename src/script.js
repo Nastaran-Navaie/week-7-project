0c908371360cecd69675ccb802ed4d78c2ee7f64
Nastaran-Navaie <156191998+Nastaran-Navaie@users.noreply.github.com>
2024-01-17 13:29:09 +0330

    search engine
    1) added a few IDs to the HTML to target them in JS
    2) the basics of search engine : prevent the page from reloading - replacing the entered city value with the h1

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
+function submitSearch(event) {
+  event.preventDefault();
+  let formInputElement = document.querySelector("#form-input");
+  let formInputElementValue = formInputElement.value;
+  let city = document.querySelector("#city-title");
+  city.innerHTML = formInputElementValue;
+}
 
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", submitSearch);
