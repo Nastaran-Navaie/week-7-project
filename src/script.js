330c88256c65775156b8ca57978caa1573799f2e
Nastaran-Navaie <156191998+Nastaran-Navaie@users.noreply.github.com>
2024-01-17 14:18:41 +0330

    API integration
    api was set up and modificated -
    show the current temperature

--- src/script.js
+++ src/script.js
@@ -1,9 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#city-title");
+  city.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3bac60fa4at706da550d1a73eo81bb0f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function submitSearch(event) {
   event.preventDefault();
   let formInputElement = document.querySelector("#form-input");
   let formInputElementValue = formInputElement.value;
-  let city = document.querySelector("#city-title");
-  city.innerHTML = formInputElementValue;
+
+  searchCity(formInputElementValue);
 }
 
 let searchFormElement = document.querySelector("#search-form");
