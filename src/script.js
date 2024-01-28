aa3d4c9ddf382fec8d103bab355e016e3245cb79
Nastaran-Navaie <156191998+Nastaran-Navaie@users.noreply.github.com>
2024-01-28 23:36:07 +0330

    finnished the 8th week project

--- src/script.js
+++ src/script.js
@@ -17,6 +17,7 @@ function refreshWeather(response) {
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji" />`;
 
   timeElement.innerHTML = formatDate(date);
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -41,7 +42,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "3bac60fa4at706da550d1a73eo81bb0f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -53,5 +54,47 @@ function submitSearch(event) {
   searchCity(formInputElementValue);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "3bac60fa4at706da550d1a73eo81bb0f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        ` 
+    <div class="weather-forecast-day">      
+     <div class="weather-forecast-date">${formatDay(day.time)}</div>
+     <div class="weather-forecast-icon">
+      <img src="${day.condition.icon_url}" />
+       </div>
+     <div class="weather-forecast-temperatures">
+     <span class="weather-forecast-temperature"> 
+     <strong> ${Math.round(day.temperature.maximum)}°  </strong>
+     </span>
+     <span class="weather-forecast-temperature"> ${Math.round(
+       day.temperature.minimum
+     )}°  </span>
+     </div>
+     </div>
+      `;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
